926415c3daa12cc116d93e5dde77133e1b437838
Thomas <tparonis@verizon.net>
2021-03-06 11:13:10 -0500

    Committing so I can develop on Macbook

--- TireTorture/TireTorture.iOS/Main.cs
+++ TireTorture/TireTorture.iOS/Main.cs
@@ -10,6 +10,7 @@ namespace TireTorture.iOS
     public class Application
     {
         // This is the main entry point of the application.
+        
         static void Main(string[] args)
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
